8f6c1137b0d7e67db32f09a9681f73ae9bb8e585
Ramdhani Akbar <ramdhaniakbarmuhamad@gmail.com>
2026-03-31 10:29:29 +0700

    feat: poster update

--- lib/pages/poster_carousel_page.dart
+++ lib/pages/poster_carousel_page.dart
@@ -6,20 +6,50 @@ const List<String> _posters = [
   'assets/images/posters/poster_3.png',
 ];
 
-class PosterCarouselPage extends StatelessWidget {
+class PosterCarouselPage extends StatefulWidget {
   const PosterCarouselPage({super.key});
+
+  @override
+  State<PosterCarouselPage> createState() => _PosterCarouselPageState();
+}
+
+class _PosterCarouselPageState extends State<PosterCarouselPage> {
+  bool _imagesReady = false;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (!_imagesReady) {
+      _precacheImages();
+    }
+  }
+
+  Future<void> _precacheImages() async {
+    await Future.wait(
+      _posters.map((path) => precacheImage(AssetImage(path), context)),
+    );
+    if (mounted) {
+      setState(() => _imagesReady = true);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: PageView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: _posters.length,
-        itemBuilder: (context, index) => SizedBox.expand(
-          child: Image.asset(_posters[index], fit: BoxFit.contain),
-        ),
-      ),
+      body: !_imagesReady
+          ? const Center(child: CircularProgressIndicator())
+          : PageView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: _posters.length,
+              itemBuilder: (context, index) => SizedBox.expand(
+                child: Image.asset(
+                  _posters[index],
+                  fit: BoxFit.contain,
+                  filterQuality: FilterQuality.medium,
+                ),
+              ),
+            ),
     );
   }
 }
