8a502425bc901cd3a3e2db4add90688c4284d8eb
Ramdhani Akbar <ramdhaniakbarmuhamad@gmail.com>
2026-03-31 07:42:15 +0700

    feat: update definition screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,16 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/services.dart';
 
 import 'pages/poster_carousel_page.dart';
 
-const MethodChannel _windowChannel = MethodChannel('app.window');
-
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   runApp(const MyApp());
-
-  doWhenWindowReady(() async {
-    final win = appWindow;
-    const initialSize = Size(2160, 3840);
-    const minimumSize = Size(2160, 3840);
-    win.size = initialSize;
-    win.minSize = minimumSize;
-    win.alignment = Alignment.center;
-    win.title = "Smart Table";
-    win.show();
-
-    if (Platform.isWindows) {
-      try {
-        await _windowChannel.invokeMethod<bool>('setFullscreen', true);
-      } on PlatformException {
-        // Ignore and keep the regular resizable window if the runner API fails.
-      }
-    }
-    win.show();
-  });
 }
 
 class MyApp extends StatelessWidget {
