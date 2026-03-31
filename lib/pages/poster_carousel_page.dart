0f6db54cd4b54b7b1ee5ffff32501db616edc6b6
Ramdhani Akbar <ramdhaniakbarmuhamad@gmail.com>
2026-03-31 07:54:24 +0700

    feat: update manual swiping

--- lib/pages/poster_carousel_page.dart
+++ lib/pages/poster_carousel_page.dart
@@ -6,43 +6,19 @@ const List<String> _posters = [
   'assets/images/posters/poster_3.png',
 ];
 
-class PosterCarouselPage extends StatefulWidget {
+class PosterCarouselPage extends StatelessWidget {
   const PosterCarouselPage({super.key});
-
-  @override
-  State<PosterCarouselPage> createState() => _PosterCarouselPageState();
-}
-
-class _PosterCarouselPageState extends State<PosterCarouselPage> {
-  late final PageController _pageController;
-
-  @override
-  void initState() {
-    super.initState();
-    _pageController = PageController();
-  }
-
-  @override
-  void dispose() {
-    _pageController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          PageView.builder(
-            controller: _pageController,
-            itemCount: _posters.length,
-            itemBuilder: (context, index) => SizedBox.expand(
-              child: Image.asset(_posters[index], fit: BoxFit.contain),
-            ),
-          ),
-        ],
+      body: PageView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: _posters.length,
+        itemBuilder: (context, index) => SizedBox.expand(
+          child: Image.asset(_posters[index], fit: BoxFit.contain),
+        ),
       ),
     );
   }
